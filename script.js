56661f8d2cd46302dfa9d70ffee71ffec6a4dc1a
GraceCoder <duquizagrace03@gmail.com>
2025-03-11 14:26:53 +0100

    Add portfolio website files

---
+++ script.js
@@ -0,0 +1,12 @@
+// script.js
+
+// Function to handle smooth scroll when clicking navigation links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
